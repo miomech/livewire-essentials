f46f56717c19a21ded597c83afafdf0710ead7ac
miomech <29011382+miomech@users.noreply.github.com>
2024-08-19 23:42:09 -0500

    getting-started understand lifecycle hooks
    Basic usage of the most commonly used lifecycle hooks 'mount()' and 'updated()'

--- app/Livewire/Todos.php
+++ app/Livewire/Todos.php
@@ -8,12 +8,34 @@ class Todos extends Component
 {
     public $todo = '';
 
-    public $todos = [
-        'Go to the store',
-        'Go to the market',
-        'Go to work',
-        'Go to the concert',
-    ];
+    public $todos = [];
+
+    // This method is called when the component is mounted
+    // This is the equivalent of the created() method in Vue
+    public function mount(): void
+    {
+        $this->todos = [
+            'Go to the store',
+            'Go to the market',
+            'Go to work',
+            'Go to the concert',
+        ];
+    }
+
+    // This lifecycle hook is called when any property is updated
+    public function updated($property, $value): void
+    {
+        // You could use an if else statement to check which property was updated
+        if ($property === 'todo') {
+            $this->$property = strtoupper($value);
+        }
+    }
+
+    // Alternatively, you could use a updatedX method where X is the property name
+//    public function updatedTodo($value): void
+//    {
+//        $this->todo = strtoupper($value);
+//    }
 
     public function add()
     {
